aae53cdfd0b5006cf1169cd2edc112ace145c167
Joshua Smith <smith.joshuasterling@gmail.com>
2020-06-04 21:35:36 -0600

    conditional required for object deconstruction

--- src/App.js
+++ src/App.js
@@ -5,26 +5,32 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [pokemon, setPokemon] = useState(
-    "https://pokeapi.co/api/v2/pokemon?limit=151"
-  );
-  const [loading, setLoading] = useState(true);
+  const [pokemon, setPokemon] = useState([]);
+  // const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
-    let cancel;
-    axios
-      .get(pokemon, {
-        cancelToken: new axios.CancelToken((c) => (cancel = c)),
-      })
-      .then((res) => {
-        setLoading(false);
-        setPokemon(res.data.results.map((p) => p.name));
-      });
-
-    return () => cancel();
+    axios.get("https://pokeapi.co/api/v2/pokemon/1").then((res) => {
+      setPokemon(res.data);
+    });
   }, []);
 
+  // useEffect(() => {
+  //   setLoading(true);
+  //   let cancel;
+  //   axios
+  //     .get(pokemon, {
+  //       cancelToken: new axios.CancelToken((c) => (cancel = c)),
+  //     })
+  //     .then((res) => {
+  //       setLoading(false);
+  //       setPokemon(res.data.results.map((p) => p.name));
+  //     });
+
+  //   return () => cancel();
+  // }, []);
+
+  console.log(pokemon);
+  console.log(pokemon.moves ? pokemon.sprites.front_default : null);
   return (
     <div className="app-container">
       <Header />
@@ -32,6 +38,25 @@ function App() {
       <div className="pokedex-container">
         <img className="something" src={"/pokedex.jpg"} alt="pokedex" />
       </div>
+      {pokemon.name}
+      <br />
+      {pokemon.order}
+      <br />
+      {pokemon.weight}
+      <br />
+      {pokemon.height}
+      <br />
+      <img
+        className="pokemon"
+        style={{ width: "300px", height: "300px" }}
+        src={pokemon.moves ? pokemon.sprites.front_default : null}
+        alt="pokemon"
+      />
+      {/* {pokemon.abilities} */}
+      {/* {pokemon.abilities[0]} */}
+      {/* {pokemon.species.name} */}
+      {/* {pokemon.stats.base_stat} */}
+      {/* <img src={pokemon.sprites.back_default} alt="current pokemon" /> */}
     </div>
   );
 }
