72163d2b4000d6ffea9e4fe652b22f1035ee3f86
Joshua Smith <smith.joshuasterling@gmail.com>
2020-07-13 20:10:21 -0600

    refactoring for performance

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
-import Search from "./components/Search/Search";
 import Pokemon from "./components/Pokemon/Pokemon";
 import Pokedex from "./components/Pokedex/Pokedex";
+import Search from "./components/Search/Search";
 import axios from "axios";
 import "./App.css";
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
-  const [currentPageUrl, setCurrentPageUrl] = useState(
-    "https://pokeapi.co/api/v2/pokemon/"
-  );
   const [counter, setCounter] = useState(Math.floor(Math.random() * 151) + 1);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
-    let cancel;
-    axios
-      .get(`${currentPageUrl}${counter}`, {
-        cancelToken: new axios.CancelToken((c) => (cancel = c)),
-      })
-      .then((res) => {
-        setLoading(false);
-        setPokemon(res.data);
-      });
-    return () => cancel();
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${counter}`).then((res) => {
+      setPokemon(res.data);
+    });
   }, [counter]);
 
   function goToPrevPokemon() {
@@ -36,32 +24,26 @@ function App() {
     setCounter(counter + 1);
   }
 
-  if (loading) return "Loading...";
-
-  console.log(pokemon);
-  // console.log(pokemon.moves ? pokemon.sprites.front_default : null);
-
+  console.log(counter);
   return (
     <div className="app-container">
       <Header />
-      <img
-        className="sliding-background"
-        src={"/wallpaper.jpg"}
-        alt="background image"
-      />
-      <img className="pokedex" src={"/pokedex.jpg"} alt="pokedex" />
+
+      {/* Background Image */}
+      <img className="background-image" src={"field.png"} alt="background" />
+
+      {/* Pokédex Image */}
+      <img className="pokedex" src={"/new-pokedex.png"} alt="pokedex" />
+
       <Pokemon
         pokemon={pokemon}
         counter={counter}
         goToNextPokemon={goToNextPokemon}
         goToPrevPokemon={goToPrevPokemon}
       />
+
       <Pokedex pokemon={pokemon} />
-      <Search
-        counter={counter}
-        goToPrevPokemon={goToPrevPokemon}
-        goToNextPokemon={goToNextPokemon}
-      />
+      <Search setCounter={setCounter} />
     </div>
   );
 }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
-export default function Search() {
+export default function Search(props) {
+  const [input, setInput] = useState("");
+
+  function handleInput(input) {
+    props.setCounter(input);
+    setInput("");
+  }
+
+  function handleChange(value) {
+    setInput(value);
+  }
+
   return (
     <div className="search-container">
-      <input
-        className="search-box"
-        placeholder="Search the Pokémon Name or Number here..."
-        type="text"
-      ></input>
+      <form onSubmit={(event) => event.preventDefault()}>
+        <input
+          value={input}
+          className="search-box"
+          placeholder="Enter the Pokémon Name or Number here..."
+          type="text"
+          onChange={(event) => handleChange(event.target.value)}
+        />
+        <button
+          type="submit"
+          className="submit-button"
+          onClick={() => handleInput(input)}
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 }
